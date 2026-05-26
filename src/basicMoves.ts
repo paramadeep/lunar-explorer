1e03c7311a59a6ffd79f30d8606195a58e4c049f
deepak <anandhadeepak@gmail.com>
2026-05-26 09:17:26 +0530

    move const to module level

--- src/basicMoves.ts
+++ src/basicMoves.ts
@@ -1,43 +1,49 @@
 import coordinate from "./coordinate"
+const rightSideMap: Record<string, string> = {
+  "N":"E",
+  "E":"S",
+  "S":"W",
+  "W":"N",
+}
+const leftSideMap: Record<string, string> = {
+  "N":"W",
+  "E":"N",
+  "S":"E",
+  "W":"S",
+}
+
+const xIncrementerMap: Record<string, number> = {
+  "N":0,
+  "S":0,
+  "E":1,
+  "W":-1,
+}
+
+const yIncrementerMap: Record<string, number> = {
+  "N":-1,
+  "S":1,
+  "E":0,
+  "W":0,
+}
 
 function getRightSide(facingDirection: string): string {
-  const rightSideMap: Record<string, string> = {
-    "N":"E",
-    "E":"S",
-    "S":"W",
-    "W":"N",
-  }
+ 
   return rightSideMap[facingDirection]
 }
 
 function getLeftSide(facingDirection: string): string {
-  const leftSideMap: Record<string, string> = {
-    "N":"W",
-    "E":"N",
-    "S":"E",
-    "W":"S",
-  }
+  
   return leftSideMap[facingDirection]
 }
 
 function getNewXIfValid(initialCoOrdinate: coordinate): number {
-  var xIncrementerMap: Record<string, number> = {
-    "N":0,
-    "S":0,
-    "E":1,
-    "W":-1,
-  }
+  
   const valueToIncrement = xIncrementerMap[initialCoOrdinate.facingDirection]  
   return initialCoOrdinate.x+valueToIncrement
 }
 
 function getNewYIfValid(initialCoOrdinate: coordinate): number {
-  var yIncrementerMap: Record<string, number> = {
-    "N":-1,
-    "S":1,
-    "E":0,
-    "W":0,
-  }
+ 
   const valueToIncrement = yIncrementerMap[initialCoOrdinate.facingDirection]  
   return initialCoOrdinate.y+valueToIncrement
 }
